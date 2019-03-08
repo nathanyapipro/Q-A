0f414b32d03e04c6dc5513caa38023d71a2ebdd4
Nathan Yapi <nathanyapi.pro@gmail.com>
2019-03-07 19:44:04 -0500

    feat(client): started work on Settings page

---
+++ client/src/pages/Settings/index.tsx
@@ -0,0 +1,122 @@
+import * as React from "react";
+import { makeStyles } from "@material-ui/styles";
+import { Theme } from "@material-ui/core/styles";
+import { compose } from "react-apollo";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+interface OwnProps {}
+
+type Props = OwnProps;
+
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  expansionPanel: {
+    display: "flex",
+    flexDirection: "column",
+    marginBottom: theme.spacing.unit * 2
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: "33.33%",
+    flexShrink: 0
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary
+  }
+}));
+
+function SettingsBase(_: Props) {
+  const classes = useStyles();
+
+  const [panelExpanded, setPanelExpanded] = React.useState<string | undefined>(
+    "workpace-configuration"
+  );
+
+  const handleChange = (panel: string) => (
+    _: React.ChangeEvent<{}>,
+    expanded: boolean
+  ) => {
+    setPanelExpanded(expanded ? panel : undefined);
+  };
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.expansionPanel}>
+        <ExpansionPanel
+          expanded={Boolean(panelExpanded === "general")}
+          onChange={handleChange("general")}
+        >
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography noWrap className={classes.heading}>
+              General
+            </Typography>
+            <Typography noWrap className={classes.secondaryHeading}>
+              Global App Level Configuration
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
+              feugiat. Aliquam eget maximus est, id dignissim quam.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </div>
+      <div className={classes.expansionPanel}>
+        <ExpansionPanel
+          expanded={Boolean(panelExpanded === "users")}
+          onChange={handleChange("users")}
+        >
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography noWrap className={classes.heading}>
+              Users
+            </Typography>
+            <Typography noWrap className={classes.secondaryHeading}>
+              Create, Update Non-Annonymous Users for the App
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Donec placerat, lectus sed mattis semper, neque lectus feugiat
+              lectus, varius pulvinar diam eros in elit. Pellentesque convallis
+              laoreet laoreet.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </div>
+      <div className={classes.expansionPanel}>
+        <ExpansionPanel
+          expanded={Boolean(panelExpanded === "workpace-configuration")}
+          onChange={handleChange("workpace-configuration")}
+        >
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography noWrap className={classes.heading}>
+              Workpace Configuration
+            </Typography>
+            <Typography noWrap className={classes.secondaryHeading}>
+              Filtering has been entirely disabled for whole web server
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
+              sit amet egestas eros, vitae egestas augue. Duis vel est augue.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </div>
+    </div>
+  );
+}
+
+const Settings: React.ComponentType<OwnProps> = compose()(SettingsBase);
+
+export default Settings;
--- client/src/routers/AppRouter.tsx
+++ client/src/routers/AppRouter.tsx
@@ -5,6 +5,7 @@ import AppLayout from "../layouts/AppLayout";
 const AskAQuestion = React.lazy(() => import("../pages/AskAQuestion"));
 const Questions = React.lazy(() => import("../pages/Questions"));
 const Question = React.lazy(() => import("../pages/Question"));
+const Settings = React.lazy(() => import("../pages/Settings"));
 
 function AppRouter() {
   return (
@@ -13,6 +14,7 @@ function AppRouter() {
         <Route path="/ask-a-question" exact component={AskAQuestion} />
         <Route path="/questions" exact component={Questions} />
         <Route path="/questions/:id" exact component={Question} />
+        <Route path="/settings" exact component={Settings} />
         <Redirect from="/" to="/questions" />
       </Switch>
     </AppLayout>
