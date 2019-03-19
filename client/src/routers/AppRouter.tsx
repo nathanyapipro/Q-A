e05683e1d49bbcae9076df443af8e1dd2210c184
Nathan Yapi <nathanyapi.pro@gmail.com>
2019-03-19 10:01:18 -0400

    fix(client): added progressive loading on bundles split

--- client/src/routers/AppRouter.tsx
+++ client/src/routers/AppRouter.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import AppLayout from "../layouts/AppLayout";
+import Loading from "../components/Loading";
 
 const AskAQuestion = React.lazy(() => import("../pages/AskAQuestion"));
 const Questions = React.lazy(() => import("../pages/Questions"));
@@ -10,13 +11,15 @@ const Question = React.lazy(() => import("../pages/Question"));
 function AppRouter() {
   return (
     <AppLayout>
-      <Switch>
-        <Route path="/ask-a-question" exact component={AskAQuestion} />
-        <Route path="/questions" exact component={Questions} />
-        <Route path="/questions/:id" exact component={Question} />
-        {/* <Route path="/settings" exact component={Settings} /> */}
-        <Redirect from="/" to="/questions" />
-      </Switch>
+      <React.Suspense fallback={<Loading />}>
+        <Switch>
+          <Route path="/ask-a-question" exact component={AskAQuestion} />
+          <Route path="/questions" exact component={Questions} />
+          <Route path="/questions/:id" exact component={Question} />
+          {/* <Route path="/settings" exact component={Settings} /> */}
+          <Redirect from="/" to="/questions" />
+        </Switch>
+      </React.Suspense>
     </AppLayout>
   );
 }
