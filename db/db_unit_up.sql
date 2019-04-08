7ce9b7aa7ea1905d515fcfb46fec45b15cd95f9a
Nathan Yapi <nathanyapi.pro@gmail.com>
2019-04-08 18:42:23 -0400

    fix(env): removed info

--- db/db_unit_up.sql
+++ db/db_unit_up.sql
@@ -1,9 +1,9 @@
 create database fundamental;
 
-create role fundamental_admin with login superuser password '12345678';
+create role fundamental_admin with login superuser password 'xxx';
 grant all privileges on database fundamental to fundamental_admin;
 
-create role fundamental_visitor with login password '12345678';
+create role fundamental_visitor with login password 'xxx';
 
 create role fundamental_unauthenticated;
 create role fundamental_authenticated;
